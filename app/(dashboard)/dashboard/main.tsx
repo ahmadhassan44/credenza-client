9b474e929f9d2e356471b712e38417126498a91e
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-05-17 14:29:52 +0500

    fix/dashboard alignment

--- app/(dashboard)/dashboard/main.tsx
+++ app/(dashboard)/dashboard/main.tsx
@@ -6,7 +6,7 @@ interface DashboardMainProps {
 
 export default function DashboardMain({ children }: DashboardMainProps) {
   return (
-    <main className="ml-[25%] flex flex-col pt-[63px] pb-[60px] px-[5%] min-w-0">
+    <main className="ml-[15vw]  flex flex-col pt-[63px] pb-[60px] px-[5%] min-w-0">
       {children}
     </main>
   );
